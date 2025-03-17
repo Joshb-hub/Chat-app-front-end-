497d096ce8f56b0598477b4b6bc264eb7106a384
Joshita Bhattacharyya <joshbhatta7@gmail.com>
2025-03-17 15:39:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showRegister() {
+    window.location.href = "sign up.html";  // Redirect to Sign Up page
+}
+
+function showLogin() {
+    window.location.href = "sign in.html";  // Redirect to Sign In page
+}
+
